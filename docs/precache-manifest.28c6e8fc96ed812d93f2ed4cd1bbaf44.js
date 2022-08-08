b056cbf9e46c40e8fa791c0890d1b144d42b8c03
Sonu Saini <sonu2311saini@gmail.com>
2022-08-08 14:44:29 +0100

    Add live demo

---
+++ docs/precache-manifest.28c6e8fc96ed812d93f2ed4cd1bbaf44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "835c1e4558cfc8b1cbbc",
+    "url": "/static/css/main.456941a6.chunk.css"
+  },
+  {
+    "revision": "835c1e4558cfc8b1cbbc",
+    "url": "/static/js/main.835c1e45.chunk.js"
+  },
+  {
+    "revision": "35e2057810228ba9f935",
+    "url": "/static/js/1.35e20578.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa94878b4177db8c60df4b79a07ee8de",
+    "url": "/index.html"
+  }
+];
